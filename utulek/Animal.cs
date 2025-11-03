d1a38c00a65c05011a7c258aecb3b7acf0eb8342
Cirax856 <Michal.Schneider07@seznam.cz>
2025-11-03 12:31:24 +0100

    Added animal class and updated .gitignore

---
+++ utulek/Animal.cs
@@ -0,0 +1,23 @@
+public class Animal
+{
+    public int id;
+    public string name;
+    public string type;
+    public int age;
+    public string gender;
+    public string date;
+    public string state;
+    public string note;
+
+    public Animal(int id, string name, string type, int age, string gender, string date, string state, string note)
+    {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.age = age;
+        this.gender = gender;
+        this.date = date;
+        this.state = state;
+        this.note = note;
+    }
+}
