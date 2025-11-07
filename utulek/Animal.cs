579509f9f4ab872b07337a028d97c8323baccb97
IngCigi <polachlukas12@gmail.com>
2025-11-07 13:29:40 +0100

    finished Animal.cs
    finished the Animal class and the animalCard() function

--- utulek/Animal.cs
+++ utulek/Animal.cs
@@ -1,3 +1,5 @@
+using System;
+
 public class Animal
 {
     public int id;
@@ -6,10 +8,12 @@ public class Animal
     public int age;
     public string gender;
     public string date;
-    public string state;
+    public string status;
     public string note;
+    public bool adoption;
+    public string adoptionDate;
 
-    public Animal(int id, string name, string type, int age, string gender, string date, string state, string note)
+    public Animal(int id, string name, string type, int age, string gender, string date, string status, string note)
     {
         this.id = id;
         this.name = name;
@@ -17,9 +21,13 @@ public class Animal
         this.age = age;
         this.gender = gender;
         this.date = date;
-        this.state = state;
+        this.status = status;
         this.note = note;
     }
 
-
+    public string animalCard()
+    {
+        string card = string.Format("{0} jménem {1} ve vìku {2} s pohlavím {3} bylo pøijato {4} ve stavu {5} s poznámkou: {6}. Adopce: {7} s datem: {8}. id: {9}", type, name, age, gender, date, status, note, adoption, adoptionDate, id);
+        return card;
+    }
 }
